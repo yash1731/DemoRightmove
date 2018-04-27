5b252dc2b240dd9dcb3d6431151c3731c9b732e3
Your Name <you@example.com>
2018-04-27 13:12:03 +0100

    new screenshot

---
+++ src/test/java/library/ScreenShot.java
@@ -0,0 +1,10 @@
+package library;
+
+public class ScreenShot {
+	
+	public void takesScreenShot()
+	{
+		System.out.println("take screen shot");
+	}
+
+}
---
+++ src/test/java/library/SendMail.java
@@ -0,0 +1,10 @@
+package library;
+
+public class SendMail {
+	
+	public void sendMail()
+	{
+		System.out.println("send mail");
+	}
+
+}
